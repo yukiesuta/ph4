af394dc23139b3f0ae43714f02b6ef22fba614b0
yukiho <matsumoto.yukiho.0704@gmail.com>
2023-02-02 01:38:08 +0900

    miner change :bump-check

--- js/index.js
+++ js/index.js
@@ -7,13 +7,11 @@ class Vec {
 }
 
 function setup() {
-    console.log('a');
     for (let i = 0; i < 24; i++) {
-        console.log(Math.floor(i/4))
-      let p = new Vec(50*(i%8)+120, 50+40*Math.floor(i/8));
-      blocks.push(new Block(p, 20));
+        let p = new Vec(50 * (i % 8) + 120, 50 + 40 * Math.floor(i / 8));
+        blocks.push(new Block(p, 20));
     }
-  }
+}
 
 // 長方形
 class Rect {
@@ -25,10 +23,11 @@ class Rect {
 
 // ボール
 class Ball extends Rect {
-    constructor(_p, _v) {
+    constructor(_p, _v,_c) {
         super(10, 10);
         this.p = _p;
         this.v = _v;
+        this.c = _c;
     }
 }
 
@@ -39,23 +38,40 @@ class Paddle extends Rect {
     }
 }
 
-class Block extends Rect{
+class Block extends Rect {
     constructor(_p, _r) {
         super(10, 10);
-      this.p = _p;
-      this.r = _r;
+        this.p = _p;
+        this.r = _r;
     }
-  }
+}
 
 
 const canvas = document.getElementById("pong");
 const context = canvas.getContext("2d");
 
+let balls = [];
 
-const ball = new Ball(
+balls[1] = new Ball(
     new Vec(1, 1),
-    new Vec(300, 300)
+    new Vec(200, 200),
+    'blue'
 );
+balls[0] = new Ball(
+    new Vec(10, 5),
+    new Vec(300, 300),
+    'red'
+);
+balls[2] = new Ball(
+    new Vec(10, 52),
+    new Vec(400, 400),
+    'green'
+);
+// balls[3] = new Ball(
+//     new Vec(10, 53),
+//     new Vec(300, 200),
+//     'yellow'
+// );
 
 const paddle = new Paddle(
     new Vec(50, 30)
@@ -83,59 +99,62 @@ document.addEventListener("mousemove", function (e) {
 });
 
 function update() {
-    ball.p.x += ball.v.x * 1 / 50;
-    ball.p.y += ball.v.y * 1 / 50;
-
     context.fillStyle = "black";
     context.fillRect(0, 0, canvas.width, canvas.height);
 
+    for (let ball of balls) {
+        ball.p.x += ball.v.x * 1 / 50;
+        ball.p.y += ball.v.y * 1 / 50;
 
-    if (ball.p.x < 0 || ball.p.x > canvas.width) {
-        ball.v.x = -ball.v.x;
-    }
-    if (ball.p.y < 0 || (ball.v.y>0&&paddle.p.x-100 < ball.p.x && paddle.p.x + 100 > ball.p.x && ball.p.y > 320)) {
-        ball.v.y = -ball.v.y;
-    }
-    if (ball.p.y > canvas.height) {
-        // context.removeChild();
-        var str = 'ゲームオーバー';
-        context.strokeStyle = 'red';              // 枠線の色
-        context.font = "50px 'ＭＳ ゴシック'";
-        context.textAlign = 'center';               // 配置
-        context.strokeText(str, 300, 50);
-        context.fillText('center', 1500, 550);
-        return false;
-    }
-    for (let block of blocks) {
-        let blockx = block.p.x; 
-        let blocky = block.p.y; 
 
-        if (blockx-10<ball.p.x&&blockx+10>ball.p.x&&blocky+10>ball.p.y&&blocky-10<ball.p.y) {
 
-          ball.v.y = -ball.v.y;
-          blocks.splice(blocks.indexOf(block), 1);
-      }
+        if (ball.p.x < 0 || ball.p.x > canvas.width) {
+            ball.v.x = -ball.v.x;
+        }
+        if (ball.p.y < 0 || (ball.v.y > 0 && paddle.p.x - 100 < ball.p.x && paddle.p.x + 100 > ball.p.x && ball.p.y > 320&& ball.p.y < 325)) {
+            ball.v.y = -ball.v.y;
+        }
+        if (ball.p.y > canvas.height-10) {
+        var str = 'ドンマイ！笑';
+        balls =[];
+        blocks=[1];
+        var myh1 = document.getElementById("myh1");
+        myh1.innerHTML = str;
+
+        }
+        for (let block of blocks) {
+            let blockx = block.p.x;
+            let blocky = block.p.y;
+
+            if (blockx - 10 < ball.p.x && blockx + 10 > ball.p.x && blocky + 10 > ball.p.y && blocky - 10 < ball.p.y) {
+
+                ball.v.y = -ball.v.y;
+                blocks.splice(blocks.indexOf(block), 1);
+            }
+        }
+        context.fillStyle = ball.c;
+        context.fillRect(ball.p.x, ball.p.y, ball.size.x, ball.size.y);
     }
-    context.fillStyle = "white";
-    context.fillRect(ball.p.x, ball.p.y, ball.size.x, ball.size.y);
+    
 
 
     context.fillStyle = "blue";
-    context.fillRect(paddle.p.x-100, 330, 200, 20);
+    context.fillRect(paddle.p.x - 100, 330, 200, 20);
 
     for (let b of blocks) {
         context.fillStyle = "red";
         context.fillRect(b.p.x, b.p.y, 20, 5);
-      }
-    if(blocks.length===10){
-        var str = 'おめでとう';
-        context.strokeStyle = 'red';              // 枠線の色
-        context.font = "50px 'ＭＳ ゴシック'";
-        context.textAlign = 'center';               // 配置
-        context.strokeText(str, 300, 50);
-        context.fillText('center', 1500, 550);
-        return false;
+    }
+    if (blocks.length < 10) {
+
+    }
+    if (blocks.length < 1) {
+        var str = 'おめでと笑';
+        balls =[];
+        blocks=[];
+        var myh1 = document.getElementById("myh1");
+        myh1.innerHTML = str;
     }
 }
-    setup();
-    callback();
+setup();
+callback();
